b210a53d28989b5e018bfd6d0dd26dd7a645f82d
Brian P. Evans <dhwyll@gmail.com>
2017-10-23 11:54:59 -0700

    Database connections to Mongo and SQL

--- models/index.js
+++ models/index.js
@@ -6,7 +6,8 @@ var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.json')[env];
-var db = {};
+var sdb = {};
+console.log("I got into index.js");
 
 if (config.use_env_variable) {
     var sequelize = new Sequelize(process.env[config.use_env_variable]);
@@ -21,16 +22,16 @@ fs
     })
     .forEach(function(file) {
         var model = sequelize['import'](path.join(__dirname, file));
-        db[model.name] = model;
+        sdb[model.name] = model;
     });
 
-Object.keys(db).forEach(function(modelName) {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
+Object.keys(sdb).forEach(function(modelName) {
+    if (sdb[modelName].associate) {
+        sdb[modelName].associate(sdb);
     }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+sdb.sequelize = sequelize;
+sdb.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = sdb;
